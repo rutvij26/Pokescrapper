3fdd12953ed4e2d17ae3a0792b1410ea5601a2a4
Rutvij Sathe <rutvij@Rutvijs-MacBook-Pro.local>
2021-09-17 19:15:04 -0400

    Admin Site setup

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
+
+class pokemon(models.Model):
+        name = models.CharField(max_length=255)
+        # Types = ArrayField(
+        #     models.CharField(max_length=255),
+        #     size=4
+        # )
+        Hp = models.BigIntegerField()
+        Attack = models.BigIntegerField()
+        Defence = models.BigIntegerField()
+        Spattack = models.BigIntegerField()
+        Spdefence = models.BigIntegerField()
+        Speed = models.BigIntegerField()
+
